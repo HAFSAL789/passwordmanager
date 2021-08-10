30ee59517b8de5e63b1aaad26d185bbccefed4d0
HAFSAL <75556327+HAFSAL789@users.noreply.github.com>
2021-08-10 13:54:59 +0530

    favicon error fix 5 added

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cv2#04jc08ed)fehz52@42n#vwvlm49kwif&a1hyoo(bivglx^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
