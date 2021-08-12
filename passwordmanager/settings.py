59825a3ed17a6ca44b100c38e304da7ce9356800
HAFSAL <75556327+HAFSAL789@users.noreply.github.com>
2021-08-12 21:58:40 +0530

    djangoheroku added

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -124,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
